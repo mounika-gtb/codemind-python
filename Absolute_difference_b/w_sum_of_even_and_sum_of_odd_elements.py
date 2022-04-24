9b135fa0bc516cf8a5b358058a850554761c5efd
mounika-gtb <84364046+mounika-gtb@users.noreply.github.com>
2022-04-24 06:15:05 +0530

    n=int(input()) sum1=0 sum2=0 l=list(map(int,input().strip().split()))[ :n] for i in range(0,len(l)):    if l[i]%2==0:       sum1=sum1+l[i]    else:       sum2=sum2+l[i] d=sum2-sum1 if d<0:     print(-d) else:     print(d)

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.py
@@ -0,0 +1,15 @@
+n=int(input())
+sum1=0
+sum2=0
+l=list(map(int,input().strip().split()))[ :n]
+for i in range(0,len(l)):
+   if l[i]%2==0:
+      sum1=sum1+l[i]
+   else:
+      sum2=sum2+l[i]
+d=sum2-sum1
+if d<0:
+    print(-d)
+else:
+    print(d)
+
