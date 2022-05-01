d549c56bf70c7a58ceb320d393fcf092cc096f12
mounika-gtb <84364046+mounika-gtb@users.noreply.github.com>
2022-05-01 06:16:00 +0530

    n=int(input()) l=list(map(int,input().strip().split()))[:n] m=[] for i in range(0,len(l)):    if l[i]%2!=0:       m.append(l[i]) for i in range(0,len(l)):    if l[i]%2==0:       m.append(l[i]) listtostring=' '.join(map(str,m)) print(listtostring)

---
+++ array_of_add_and_even_elements.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().strip().split()))[:n]
+m=[]
+for i in range(0,len(l)):
+   if l[i]%2!=0:
+      m.append(l[i])
+for i in range(0,len(l)):
+   if l[i]%2==0:
+      m.append(l[i])
+listtostring=' '.join(map(str,m))
+print(listtostring)
