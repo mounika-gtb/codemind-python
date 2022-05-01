db300f2b730494dffe2321bca2bdde8c0230a084
mounika-gtb <84364046+mounika-gtb@users.noreply.github.com>
2022-05-01 13:00:37 +0530

    n=int(input()) l=list(map(int,input().strip().split()))[:n] s=0 c=0 for i in range(0,len(l)):     s=s+l[i] avg=s//n for i in range(0,len(l)):     if l[i]<=avg:         c+=1 print(c)

---
+++ elements_smaller_than_average.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().strip().split()))[:n]
+s=0
+c=0
+for i in range(0,len(l)):
+    s=s+l[i]
+avg=s//n
+for i in range(0,len(l)):
+    if l[i]<=avg:
+        c+=1
+print(c)
