ff9246b3993d3556caee08bd515aa4983301c0fe
mounika-gtb <84364046+mounika-gtb@users.noreply.github.com>
2022-05-03 21:05:41 +0530

    def fun(n):     count=0     for i in range(1,n+1):         if n%i==0:             count+=1     return count n=int(int(input())) l=list(map(int,input().strip().split()))[:n] cf=0 for i in range(0,len(l)):     c=fun(l[i])     if c==2:         cf+=1 print(cf)

---
+++ Primes_in_the_array.py
@@ -0,0 +1,14 @@
+def fun(n):
+    count=0
+    for i in range(1,n+1):
+        if n%i==0:
+            count+=1
+    return count
+n=int(int(input()))
+l=list(map(int,input().strip().split()))[:n]
+cf=0
+for i in range(0,len(l)):
+    c=fun(l[i])
+    if c==2:
+        cf+=1
+print(cf)
