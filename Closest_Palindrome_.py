cd3dc91ea44bf00e91d27647b867be77c5277f3a
mounika-gtb <84364046+mounika-gtb@users.noreply.github.com>
2022-10-23 17:23:38 +0530

    def fun(n):     e=n     s=0     while n!=0:         r=n%10         s=s*10+r         n=n//10     if e==s:         return 1     else:         return 0
    n=int(input())
    w=n
    for i in range(n-1,0,-1):
        if fun(i)==1:
            op=i
            break
    for i in range(n+1,n+n):
        if fun(i)==1:
            op1=i
            break
    d=abs(op-w)
    d1=abs(op1-w)
    if d<d1:
        print(op)
    elif d>d1:
        print(op1)
    else:
        print(op,op1)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,30 +1,30 @@
-def palindrome(n):
-   sum=0
-   while n:
-      r=n%10
-      sum=sum*10+r
-      n=n//10
-   return sum
+def fun(n):
+    e=n
+    s=0
+    while n!=0:
+        r=n%10
+        s=s*10+r
+        n=n//10
+    if e==s:
+        return 1
+    else:
+        return 0
+
 n=int(input())
-m=n
-d1=palindrome(n)
-for i in range(m-1,1,-1):
-   d2=palindrome(i)
-   if d2==i:
-      c1=i
-      break
-for i in range(m+1,m+m):
-   d3=palindrome(i)
-   if d3==i:
-      c2=i
-      break
-m1=m-c1
-m2=c2-m
-if m1<m2:
-   print(c1)
-elif m1>m2:
-   print(c2)
-elif m1==m2:
-   print(c1,c2)
-      
-            
+w=n
+for i in range(n-1,0,-1):
+    if fun(i)==1:
+        op=i
+        break
+for i in range(n+1,n+n):
+    if fun(i)==1:
+        op1=i
+        break
+d=abs(op-w)
+d1=abs(op1-w)
+if d<d1:
+    print(op)
+elif d>d1:
+    print(op1)
+else:
+    print(op,op1)
